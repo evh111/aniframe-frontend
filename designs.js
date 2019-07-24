2182dfac6e81b6eb1a6dd419643d13af341a6d60
Elijah Helmandollar <elijahhelmandollar101@gmail.com>
2019-07-23 20:46:24 -0400

    First Commit

---
+++ designs.js
@@ -0,0 +1,35 @@
+/* jshint esnext: true */
+/* global document */
+let canvas = document.getElementById('pixel_canvas');
+let height = document.getElementById('input_height');
+let width = document.getElementById('input_width');
+let sizePicker = document.getElementById('sizePicker');
+let color = document.getElementById('colorPicker');
+
+color.addEventListener('click', function() {});
+
+sizePicker.onsubmit = function(event) {
+  event.preventDefault();
+  clearGrid();
+  makeGrid();
+};
+
+function makeGrid() {
+  for (let r = 0; r < 16; r++) {
+    const row = canvas.insertRow(r);
+    for (let c = 0; c < 32; c++) {
+      const cell = row.insertCell(c);
+      cell.addEventListener('click', fillSquare);
+    }
+  }
+}
+
+function clearGrid() {
+  while (canvas.firstChild) {
+    canvas.removeChild(canvas.firstChild);
+  }
+}
+
+function fillSquare() {
+  this.setAttribute('style', `background-color: ${color.value}`);
+}
